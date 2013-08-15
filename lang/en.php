889a64bfb108df9ae818be224ce6adcc5ecd30f8
rfool <r.frunzke@manjadigital.de>
2013-08-15 20:03:34 +0200

    Create en.php

---
+++ lang/en.php
@@ -0,0 +1,17 @@
+<?php 
+/**
+ * Piwik - Open source web analytics
+ * 
+ * @link http://piwik.org
+ * @license http://www.gnu.org/licenses/gpl-3.0.html Gpl v3 or later
+ * @version $Id: $
+ * 
+ * @category Piwik_Plugins
+ * @package Piwik_WhoisLive
+ */
+
+$translations = array(
+	'WhoisLive_PluginDescription' => 'WhoisLive Plugin: This plugin exports a widget for querying whois records of the latest visitors ip addresses',
+	'WhoisLive_widgets' => 'WhoisLive',
+	'WhoisLive_widget' => 'Live Whois',
+);
